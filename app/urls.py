192d3655f3cb09d0b83bdcc25b7367f24a37259a
issyzac <issyzac.iz@gmail.com>
2020-05-18 23:22:18 +0300

    added child details screen url

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,9 @@ from django.urls import path, re_path
 from app import views
 
 urlpatterns = [
+
+    path('kid-details/<str:child_id>/', views.kid_details),
+
     # Matches any html file 
     re_path(r'^.*', views.pages, name='pages'),
 
